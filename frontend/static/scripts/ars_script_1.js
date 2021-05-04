096751446db709d761f3ca6a46815fee00625867
KrissLa <arsavit@gmail.com>
2021-05-04 15:55:03 +0300

    fix 100vh sections

---
+++ frontend/static/scripts/ars_script_1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const vh = document.querySelectorAll('.vh');
+
+const fixVhHeight = () => {
+    console.log('sdfsdfsdfd');
+    if (document.documentElement.clientWidth < 768) {
+        vh.forEach(e => {
+            console.log(e.offsetHeight);
+            e.style.height = `${e.offsetHeight}px`;
+        })
+    }
+}
+
+fixVhHeight();
+
+// document.addEventListener('resize', fixVhHeight);
