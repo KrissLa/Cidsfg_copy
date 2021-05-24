6c2c62d384676f95314c6491e9bacf96d8512b3c
KrissLa <arsavit@gmail.com>
2021-05-24 10:36:35 +0300

    added video preview

--- frontend/static/scripts/about.js
+++ frontend/static/scripts/about.js
@@ -4,3 +4,14 @@ const capacityCards = document.querySelectorAll('.hd_item.big');
 capacityCards.forEach(e => {
     e.addEventListener('click', (el) => el.preventDefault())
 })
+
+
+document.querySelector('#video-preview').addEventListener('click', e => {
+    e.target.style.display = 'none';
+    const videoDiv = `<div class="entry-video embed-responsive embed-responsive-16by9">
+                        <video controls autoplay>
+                            <source src="/static/video/Rolik_zavod.mp4" type="video/mp4">
+                        </video>
+                    </div>`;
+    e.target.parentNode.insertAdjacentHTML('afterbegin', videoDiv);
+})
