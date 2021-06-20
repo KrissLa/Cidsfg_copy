8ebd602063a10e89087fbf4a2b8e24e762717bbb
KrissLa <arsavit@gmail.com>
2021-06-20 17:26:26 +0300

    layout: big refactoring

--- frontend/static/scripts/components/about.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#video-preview').addEventListener('click', e => {
-    e.target.style.display = 'none';
-    const videoDiv = `<div class="entry-video embed-responsive embed-responsive-16by9">
-                        <video controls autoplay>
-                            <source src="/static/video/Rolik_zavod.mp4" type="video/mp4">
-                        </video>
-                    </div>`;
-    e.target.parentNode.insertAdjacentHTML('afterbegin', videoDiv);
-})
