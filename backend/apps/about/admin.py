d76035978cecdc4c843750a7041c45a62a3a0f98
KrissLa <arsavit@gmail.com>
2021-06-30 01:54:43 +0300

    added headings  and capacity to the AboutAdmin

--- backend/apps/about/admin.py
+++ backend/apps/about/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from ckeditor.widgets import CKEditorWidget
+from django import forms
 
 from .models import About
+
+
+class CapacityBodyAdminCKEditorForm(forms.ModelForm):
+    capacity_body = forms.CharField(required=False, widget=CKEditorWidget(),
+                                    label='Текст секции "Производственные мощности"')
+
+    class Meta:
+        model = About
+        fields = ('capacity_body',)
 
 
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
     """ Управление страницей Завод из панели администратора """
     save_on_top = True
+    form = CapacityBodyAdminCKEditorForm
     fieldsets = (
         ('SEO', {
             'fields': (
@@ -18,14 +30,23 @@ class AboutAdmin(admin.ModelAdmin):
         }),
         ('О компании', {
             'fields': (
+                ('about_company_title',),
                 ('about_company',),
                 ('about_company_picture',),
             )
         }),
         ('О производстве', {
             'fields': (
+                ('about_factory_title',),
                 ('about_factory',),
                 ('about_factory_picture',),
+            )
+        }),
+        ('Производственные мощности', {
+            'fields': (
+                ('capacity_title',),
+                ('capacity_sub_title',),
+                ('capacity_body',),
             )
         }),
     )
--- backend/apps/about/models.py
+++ backend/apps/about/models.py
@@ -13,6 +13,7 @@ def generate_picture_path(instance, filename):
 
 class About(SeoAbstract):
     """ Запись с информацией о производстве """
+    about_company_title = models.CharField('Заголовок секции о компании', max_length=100, default='О КОМПАНИИ')
     about_company = models.TextField('О компании')
     about_company_picture = StdImageField('Изображение О компании', upload_to=generate_picture_path,
                                           variations={
@@ -20,13 +21,22 @@ class About(SeoAbstract):
                                               'admin': (90, 60),
                                               'thumb': (15, 10),
                                           }, )
+    about_factory_title = models.CharField('Заголовок секции о производстве', max_length=100, default='О ПРОИЗВОДСТВЕ')
     about_factory = models.TextField('О производстве')
     about_factory_picture = StdImageField('Изображение О производстве', upload_to=generate_picture_path,
                                           variations={
                                               'w3000': (3000, 2000),
+                                              'w480': (480, 320),
                                               'admin': (90, 60),
                                               'thumb': (15, 10),
                                           }, )
+
+    capacity_title = models.CharField('Заголовок секции "Производственные мощности"', max_length=100,
+                                      default='ПРОИЗВОДСТВЕННЫЕ МОЩНОСТИ')
+    capacity_sub_title = models.CharField('Подзаголовок секции "Производственные мощности"', max_length=600,
+                                          default='Общая площадь производственных цехов составляет более 3 500 м² в том'
+                                                  ' числе:')
+    capacity_body = models.TextField('Текст секции "Производственные мощности"', null=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
