9eb81b2a6df3ad9cd96e57a25429e3e062f65c12
KrissLa <arsavit@gmail.com>
2021-08-03 13:34:55 +0300

    lineup: fix price

--- backend/apps/home_page/views.py
+++ backend/apps/home_page/views.py
@@ -12,7 +12,7 @@ class HomePageView(View):
 
     def get(self, request, *args, **kwargs):
         try:
-            home_data = HomePage.objects.all().order_by('-id')[0]
+            home_data = HomePage.objects.last()
         except Exception:
             home_data = None
         return render(request, self.template_name, {'home_data': home_data})
